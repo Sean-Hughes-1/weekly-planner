dc5c749dd6a2f48a3a0ab0bf7b4a9a20154202b5
John-bess4 <jbesss247@gmail.com>
2023-11-17 22:29:31 +0000

    dom row creation

--- main.js
+++ main.js
@@ -13,14 +13,21 @@ $cancelButton.addEventListener('click', (event) => {
 
 function renderEntry(entry) {
   const $tr = document.createElement('tr');
-  const $td = document.createElement('td');
-  const $editBoxTd = document.createElement('td');
-  $editBoxTd.setAttribute('class', 'edit-box');
-  const $button = document.createElement('button');
-  const $editButton = $button.setAttribute('class', 'edit-button');
-  const $deleteButton = $button.setAttribute('class', 'delete-button' );
-  $tbody.appendChild($tr);
-  $td.textContent = entry.time;
-  $tr.appendChild('$td')
+  const $tdTime = document.createElement('td');
+  $tdTime.setAttribute('id','time');
+  $tdTime.textContent = entry.time;
+  $tr.appendChild($tdTime);
+  const $tdEvent = document.createElement('td');
+  $tdEvent.setAttribute('id','event');
+  $tdEvent.textContent = entry.event;
+  $tr.appendChild($tdEvent);
+  // const $editBoxTd = document.createElement('td');
+  // $editBoxTd.setAttribute('class', 'edit-box');
+  // const $button = document.createElement('button');
+  // const $editButton = $button.setAttribute('class', 'edit-button');
+  // const $deleteButton = $button.setAttribute('class', 'delete-button');
+  // $td.textContent = entry.time;
+  // $td.textContent = entry.event;
+  // $tr.appendChild($td);
   return $tr;
 }
