a91182f659b1e31226488e8bbfbdd4304e966faf
Sean Hughes <coronapilot69@gmail.com>
2023-11-17 22:05:22 +0000

    started render entry function

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 const $addNewEvent = document.querySelector('.add-event');
 const $overlay = document.querySelector('.overlay');
-
-
+const $cancelButton = document.querySelector('.cancel');
+const $confirmButton = document.querySelector('.confirm');
+const $tbody = document.querySelector('tbody');
 $addNewEvent.addEventListener('click', function(event){
   $overlay.className = 'overlay';
+});
+
+$cancelButton.addEventListener('click', (event) => {
+  $overlay.className = 'overlay hidden';
 })
+
+function renderEntry(entry) {
+  const $tr = document.createElement('tr');
+  const $td = document.createElement('td');
+  const $editBoxTd = document.createElement('td');
+  $editBoxTd.setAttribute('class', 'edit-box');
+  const $button = document.createElement('button');
+  const $editButton = $button.setAttribute('class', 'edit-button');
+  const $deleteButton = $button.setAttribute('class', 'delete-button' );
+  $tbody.appendChild($tr);
+  $td.textContent = entry.time;
+  $tr.appendChild('$td')
+  return $tr;
+}
