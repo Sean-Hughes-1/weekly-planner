db8dd04fed91d3f10fcb8b5d08e8b5a138d0d29a
Paxton Rosenberg <rosenbergpaxton@gmail.om>
2023-11-17 22:55:08 +0000

    weekly planner commit 8

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const $overlay = document.querySelector('.overlay');
 const $cancelButton = document.querySelector('.cancel');
 const $confirmButton = document.querySelector('.confirm');
 const $tbody = document.querySelector('tbody');
+const $dayOfWeekDropDown = document.getElementById('days');
+const $entryForm = document.querySelector('.popupForm');
+
 $addNewEvent.addEventListener('click', function(event){
   $overlay.className = 'overlay';
 });
@@ -21,13 +24,24 @@ function renderEntry(entry) {
   $tdEvent.setAttribute('id','event');
   $tdEvent.textContent = entry.event;
   $tr.appendChild($tdEvent);
-  // const $editBoxTd = document.createElement('td');
-  // $editBoxTd.setAttribute('class', 'edit-box');
-  // const $button = document.createElement('button');
-  // const $editButton = $button.setAttribute('class', 'edit-button');
-  // const $deleteButton = $button.setAttribute('class', 'delete-button');
-  // $td.textContent = entry.time;
-  // $td.textContent = entry.event;
-  // $tr.appendChild($td);
+
+  const $editBoxTd = document.createElement('td');
+  $editBoxTd.setAttribute('class', 'edit-box');
+  $tr.appendChild($editBoxTd);
+
+  const $editButton = document.createElement('button');
+  $editButton.setAttribute('class', 'edit-button');
+  $editBoxTd.appendChild($editButton);
+
+  const $deleteButton = document.createElement('button');
+   $deleteButton.setAttribute('class', 'delete-button');
+  $editBoxTd.appendChild($deleteButton);
+
   return $tr;
 }
+
+$confirmButton.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const $timeOfEvent = $entryForm.elements[0].value;
+  console.log($timeOfEvent);
+})
